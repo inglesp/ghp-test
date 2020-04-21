26a60d12a9800872e485cf3e41a6c7e73c2c4d04
Peter Inglesby <peter.inglesby@gmail.com>
2020-04-22 00:00:57 +0100

    s/output/docs/

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import datetime, os
 
-os.makedirs("output", exist_ok=True)
+os.makedirs("docs", exist_ok=True)
 
-with open("output/index.html", "w") as f:
+with open("docs/index.html", "w") as f:
     f.write(f"Built at {datetime.datetime.now()}")
